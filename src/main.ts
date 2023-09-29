17b0a7871206fb0ba375656dcf450d14bd6a611a
Ignacio Valencia <86011934+ignacio-valencia@users.noreply.github.com>
2023-09-28 22:06:11 -0300

    first push

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,21 @@
-import * as core from '@actions/core'
-import { wait } from './wait'
+import * as core from '@actions/core';
+import * as fs from 'fs';
+import * as path from 'path';
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function run(): Promise<void> {
+async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const file1Path: string = core.getInput('file1');
+    const file2Path: string = core.getInput('file2');
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const file1Content: object = JSON.parse(fs.readFileSync(path.resolve(file1Path), 'utf8'));
+    const file2Content: object = JSON.parse(fs.readFileSync(path.resolve(file2Path), 'utf8'));
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const areEqual: boolean = JSON.stringify(file1Content) === JSON.stringify(file2Content);
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('areEqual', areEqual.toString());
   } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
+
+run();
